60c7ad159600cfe6e6670ab17bdee1b9eb7a29d6
thebrotad <mackie8@gmail.com>
2026-09-07 15:04:00 -0400

    Revert "FAIL access wall clock via start menu"
    This reverts commit f143d462ab325d00ac1e72d425a3d50bff3e3a46.

--- include/strings.h
+++ include/strings.h
@@ -254,7 +254,6 @@ extern const u8 gText_MenuPlayer[];
 extern const u8 gText_MenuSave[];
 extern const u8 gText_MenuOption[];
 extern const u8 gText_MenuExit[];
-extern const u8 gText_MenuTime[];
 extern const u8 gText_MenuRetire[];
 extern const u8 gText_MenuRest[];
 extern const u8 gText_Floor1[];
